7e046e06109d481971dc9ea6084d031d5f20aace
Arie May Wibowo <33383008+ariemay@users.noreply.github.com>
2018-04-02 15:06:11 +0700

    Removing unimportant comments

--- methods.py
+++ methods.py
@@ -5,27 +5,6 @@ class Method:
         position.append(input("Position: "))
         base.append(input("Base: "))
 
-    # def widthName(self, name):
-    #     nameWidth = len(name[idx])
-    #     if nameWidth > 9:
-    #         return 9 - nameWidth
-    #     else:
-    #         return 9
-    #
-    # def widthPosition(self, position):
-    #     positionWidth = len(position[idx])
-    #     if positionWidth > 9:
-    #         return 9 - positionWidth
-    #     else:
-    #         return 9
-    #
-    # def widhtBase(self, base):
-    #     baseWidth = len(base[idx])
-    #     if baseWidth > 9:
-    #         return 9 - baseWidth
-    #     else:
-    #         return 9
-
     def showData(name, position, base):
         print("| No | Employee ", " " * 10, "| Position", " " * 10, " | Base ", " " * 10, "|")
         no = 0
